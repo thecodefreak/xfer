55cafbe5eedd2dcb067adc9e95e5aaf9e8e73eec
thecodefreak <amaljohnronkha@protonmail.com>
2026-03-05 08:40:47 +0000

    fix: return 201 status on session creation

--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -63,6 +63,7 @@ func (s *Server) handleCreateSession(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(resp)
 }
 
